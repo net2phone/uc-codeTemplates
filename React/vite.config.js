b4bf81ea5b5528bdee6754487b6f881de32a806a
francocampa <fcampanella@integraccs.com>
2023-01-03 09:24:25 -0300

    create react vite project

---
+++ React/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:process.env.NODE_ENV !== "production" ? "/test" : "/forms/",
+  server:{
+    host:true,
+    port:8081,
+    // origin: "https://fcampanella.ucontactcloud.com/test",
+    fs: {
+      strict:false,
+      allow:['.']
+    },
+    hmr:{
+      host:".ucontactcloud.com/test/ws"
+    }
+  }
+})
